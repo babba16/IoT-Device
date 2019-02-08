e09c74485bf57326116e5d982b626d60705dc930
babba16 <33516147+babba16@users.noreply.github.com>
2019-02-08 17:30:59 +0000

    Got recieving messages done

--- src/messageDecoder.py
+++ src/messageDecoder.py
@@ -1,31 +1,40 @@
 import paho.mqtt.client as mqtt
+import time
 
 client = mqtt.Client()
 client.connect("test.mosquitto.org",port=1883)
 
-#------------receiving messages---------------
+msg_recieved = None
+
+#------------receiving messages---------------#
 def on_message(client,userdata,message) :
 	print("Received message:{} on topic {}".format(message.payload, message.topic))
-	return message.payload
-
-def messageDecoder(): 
-	client.on_message = on_message
-	client.subscribe("IC.embedded/BGJR/#")
-	client.loop_start()
-
-	if client.on_message == 'no':
-		client.loop_stop()
+	global msg_recieved
+	msg_recieved = (message.payload).decode("utf-8")
+	client.loop_stop()
+	
+def compareCases(msg):
+	if msg == 'no':
 		return False
 	else:
-		client.loop_stop()
 		return True
 		
+def messageDecoder(): 
+	client.on_message = on_message
+	client.subscribe("IC.embedded/BGJ/#")
+	client.loop_start()
+	time.sleep(15)
+	return compareCases(msg_recieved)
+	
 		
-#------------sending messages---------------		
+		
+#------------sending messages---------------#		
 def sendMessage(message): #sending messages
 	print(message) #testing
-	MSG_INFO = client.publish("IC.embedded/BGJR/test",message)
-	mqtt.error_string(MSG_INFO.rc)
+	MSG_INFO = client.publish("IC.embedded/BGJ/test",message)
+	#mqtt.error_string(MSG_INFO.rc)
 	#client.tls_set(ca_certs="mosquitto.org.crt",certfile="client.crt",keyfile="client.key")
 
-
+#---testing stuff----#
+#sendMessage("Foo")
+#print(messageDecoder())
