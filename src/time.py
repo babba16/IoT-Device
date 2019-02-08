ab3d9c2c0e700ff1fe2ca2069bb04ee20147a6c5
g-ramage <47153379+g-ramage@users.noreply.github.com>
2019-02-08 14:44:53 +0000

    Update time.py

--- src/time.py
+++ src/time.py
@@ -1,35 +1,23 @@
 # prompt user to feed the dog @ 8am, 12pm, 8pm
-# def problems with this in terms of implementing with the rest
-# of the code so this is just a basis i guess 
 
 import time
-
-# prompts user to feed dog y/n
-def prompt_feed ():
-	if input("Would you like to feed now?") == 'y':
-		return True
-	else:
-		return False
-#	print("newfeedingtime: " + str(newfeedingtime))
 
 # function to check set feeding times
 def feeding_time(newday, newfeedingtime):
 
 	# prompt user to feed dog at 8am, 12pm, 8pm
 	if current_time == "08:00:00":
-		print("It's breakfast time for your dog")
-		newfeedingtime = prompt_feed()
+		sendMessage("It's breakfast time for your dog")
+		newfeedingtime = True
 		if newfeedingtime == True:
 			newday = True    # newday set to true upon first feeding
 #			print("newday: " + str(newday))
 	elif current_time == "12:00:00":
-		print("It's lunch time for your dog")
-		newfeedingtime = prompt_feed()
+		sendMessage("It's lunch time for your dog")
+		newfeedingtime = True
 	elif current_time == "20:00:00":
-		print("It's dinner time for your dog")
-		newfeedingtime = prompt_feed()
-
-
+		sendMessage("It's dinner time for your dog")
+		newfeedingtime = True
 
 # main code
 print("Start")
@@ -37,8 +25,9 @@ print("Start")
 while 1:
 
 	# have time as a global variable
-	current_time = time.strftime("%H:%M:%S")
+	current_time = time.strftime("%H:%M:%S") 
 	newday = False
 	newfeedingtime = False
 	# poll function
 	feeding_time(newday, newfeedingtime)
+
