a618d84b278576f5092c301746b40c655f3b4d5c
09tangriro <47002898+09tangriro@users.noreply.github.com>
2019-02-10 12:46:55 +0000

    include json in sending messages

--- src/messageDecoder.py
+++ src/messageDecoder.py
@@ -1,5 +1,6 @@
 import paho.mqtt.client as mqtt
 import time
+import json
 
 client = mqtt.Client()
 client.connect("test.mosquitto.org",port=1883)
@@ -21,7 +22,7 @@ def compareCases(msg):
 		
 def messageDecoder(): 
 	client.on_message = on_message
-	client.subscribe("IC.embedded/BGJ/#")
+	client.subscribe("IC.embedded/BGJR/#")
 	client.loop_start()
 	time.sleep(15)
 	return compareCases(msg_recieved)
@@ -29,9 +30,11 @@ def messageDecoder():
 		
 		
 #------------sending messages---------------#		
-def sendMessage(message): #sending messages
+def sendMessage(message,food): #sending messages
 	print(message) #testing
-	MSG_INFO = client.publish("IC.embedded/BGJ/test",message)
+	data_dict = dict(time = time.ctime(), load = (message,food))
+	data_out = json.dumps(data_dict)
+	MSG_INFO = client.publish("IC.embedded/BGJR/test",message)
 	#mqtt.error_string(MSG_INFO.rc)
 	#client.tls_set(ca_certs="mosquitto.org.crt",certfile="client.crt",keyfile="client.key")
 
