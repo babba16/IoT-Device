4972e5eff864ef303279a0555dc064f5c95d7f9e
09tangriro <47002898+09tangriro@users.noreply.github.com>
2019-02-11 18:52:17 +0000

    more features

--- src/feeding_time.py
+++ src/feeding_time.py
@@ -11,24 +11,31 @@ def feeding_time(settings, foodInDay, dayLimit):
 	# prompt user to feed dog at 8am, 12pm, 8pm
 	if current_time != "15:31:00":
 		dayLimit = maxDayLimit - bowlFood(settings)
-		sendMessage("It's breakfast time for your dog, would you like to feed your dog? ", foodInDay, dayLimit)
+		sendMessageStats("It's breakfast time for your dog, would you like to feed your dog? ", 0, 3)
 		newday = True
 		newfeedingtime = messageDecoder()
 		foodInDay = foodTracker(settings, newfeedingtime, newday, foodInDay, dayLimit)
+		SendMessageMeal("Stats for this meal time, food dispensed and food left to be dispensed today:", foodInDay, dayLimit)
 		
 	elif current_time == "12:00:00":
 		dayLimit = dayLimit - foodInDay
-		sendMessage("It's lunch time for your dog, would you like to feed your dog? ", foodInDay, dayLimit)
+		foodEaten = 1-bowlFood(settings)
+		foodLeft = maxDayLimit - foodEaten
+		sendMessageStats("It's lunch time for your dog, would you like to feed your dog? ", foodEaten, foodLeft)
 		newday = False
 		newfeedingtime = messageDecoder()
 		foodInDay = foodTracker(settings, newfeedingtime, newday, foodInDay, dayLimit)
+		SendMessageMeal("Stats for this meal time, food dispensed and food left to be dispensed today:", foodInDay, dayLimit)
 		
 	elif current_time == "20:00:00":
 		dayLimit = dayLimit - foodInDay
-		sendMessage("It's dinner time for your dog, would you like to feed your dog? ", foodInDay, dayLimit)
+		foodEaten = 2-bowlFood(settings)
+		foodLeft = maxDayLimit - foodEaten
+		sendMessageStats("It's dinner time for your dog, would you like to feed your dog? ", foodEaten, foodLeft)
 		newday = False
 		newfeedingtime = messageDecoder()
 		foodInDay = foodTracker(settings, newfeedingtime, newday, foodInDay, dayLimit)
+		SendMessageMeal("Stats for this meal time, food dispensed and food left to be dispensed today:", foodInDay, dayLimit)
 		
 	feeding_time(settings, foodInDay, dayLimit)
 		
