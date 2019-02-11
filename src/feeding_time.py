f1b45d26f4d3a4a7931da46f4cc0a90c978f0338
09tangriro <47002898+09tangriro@users.noreply.github.com>
2019-02-11 16:38:52 +0000

    needs testing

--- src/feeding_time.py
+++ src/feeding_time.py
@@ -3,26 +3,27 @@ from messageDecoder import *
 from dispensing import *
 import time
 
+
 # function to check set feeding times
-def feeding_time(newday, newfeedingtime, foodInDay):
+def feeding_time(settings, foodInDay):
 	current_time = time.strftime("%H:%M:%S")
 	# prompt user to feed dog at 8am, 12pm, 8pm
 	if current_time != "15:31:00":
 		sendMessage("It's breakfast time for your dog, would you like to feed your dog? ", foodInDay)
 		newday = True
 		newfeedingtime = messageDecoder()
-		foodTracker(newfeedingtime, newday)
+		foodInDay = foodTracker(settings, newfeedingtime, newday, foodInDay)
 	elif current_time == "12:00:00":
 		sendMessage("It's lunch time for your dog, would you like to feed your dog? ", foodInDay)
 		newday = False
 		newfeedingtime = messageDecoder()
-		foodTracker(newfeedingtime, newday)
+		foodInDay = foodTracker(settings, newfeedingtime, newday, foodInDay)
 		
 	elif current_time == "20:00:00":
 		sendMessage("It's dinner time for your dog, would you like to feed your dog? ", foodInDay)
 		newday = False
 		newfeedingtime = messageDecoder()
-		foodTracker(newfeedingtime, newday)
+		foodInDay = foodTracker(settings, newfeedingtime, newday, foodInDay)
 		
 
 # main code
