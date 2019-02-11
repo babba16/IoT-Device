b3422eeea15003f43dce8bd4a7a1be2e9128c482
09tangriro <47002898+09tangriro@users.noreply.github.com>
2019-02-11 18:59:02 +0000

    allows user to input what type of dog they have

--- src/settings.py
+++ src/settings.py
@@ -1,5 +1,6 @@
 from messageDecoder import *
 
+import json
 import paho.mqtt.client as mqtt
 
 def scalerSetting():
@@ -7,7 +8,12 @@ def scalerSetting():
 	client = mqtt.Client()
 	client.connect("test.mosquitto.org",port=1883)
 
-	MSG_INFO = client.publish("IC.embedded/BGJR/test","Type what size dog you have, small medium or large:")
+	MSG_INFO = client.publish("IC.embedded/BGJR/test", "Type what size dog you have, small medium or large:")
+	mqtt.error_string(MSG_INFO.rc)
+	
+	data = dict(small = "0.5kg a day = 3 units, 1 unit per meal", medium = "1kg a day = 3 units, 1 unit per meal", large = "1.5kg a day = 3 units, 1 unit per meal")
+	data_out = json.dumps(data)
+	MSG_INFO = client.publish("IC.embedded/BGJR/test", data_out)
 	mqtt.error_string(MSG_INFO.rc)
 
 	if messageDecoder() == 1:
