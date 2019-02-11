9a66e6595497e87b429a9649a227156003fcd313
09tangriro <47002898+09tangriro@users.noreply.github.com>
2019-02-11 17:34:22 +0000

    needs testing

--- src/dispensing.py
+++ src/dispensing.py
@@ -3,35 +3,30 @@ import time
 
 bus = smbus.SMBus(1)
 mealLimit = 1
-maxDayLimit = 3
-scaler = 0 #needs to be updated
 
 bus.write_i2c_block_data(0x48,0x01,[0x88, 0x83]) # configure sensor
 
-def foodTracker(settings, newfeedingtime, newday, foodInDay):
-	global scaler = settings
+def foodTracker(settings, newfeedingtime, newday, foodInDay, dayLimit):
 	if newday == True:
 		print("It's a new day")
-		dayLimit = maxDayLimit - bowlFood()
 		foodInDay = 0
 		
 	if newfeedingtime == True and dayLimit > 0:
 		print ("Food time!")
-		dayLimit = dayLimit - dispenseFood()
-		foodInDay = foodInDay + dispenseFood()
+		foodInDay = foodInDay + dispenseFood(settings)
 		
 	return foodInDay
 		
-def dispenseFood(): #dispenses required amount of food for meal and returns the amount dispensed in thisMeal
-	thisMeal = mealLimit - bowlFood()
+def dispenseFood(settings): #dispenses required amount of food for meal and returns the amount dispensed in thisMeal
+	thisMeal = mealLimit - bowlFood(settings)
 	#TODO: needs to be drive LED to represent motor
-	while  bowlFood() < thisMeal:
-		print(bowlFood()) #for testing, allows us to continuously see the value of the weight applied to sensor. 
+	while  bowlFood(settings) < thisMeal:
+		print(bowlFood(settings)) #for testing, allows us to continuously see the value of the weight applied to sensor. 
 		time.sleep(5)
 	return thisMeal
 	
 	
-def bowlFood(): # go to sensor to get the weight of the food in the bowl
+def bowlFood(scalar): # go to sensor to get the weight of the food in the bowl
 	#read data from ADC
 	data = int.from_bytes(bus.read_i2c_block_data(0x48,0x00,2),"big")
 	scaledData = (65531-data)/scaler
