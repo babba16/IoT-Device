f36ad5d85a2a57dce8269d2d3b9f302fe6d02c23
09tangriro <47002898+09tangriro@users.noreply.github.com>
2019-02-11 17:35:14 +0000

    needs testing

--- src/feeding_time.py
+++ src/feeding_time.py
@@ -3,29 +3,34 @@ from messageDecoder import *
 from dispensing import *
 import time
 
+maxDayLimit = 3
 
 # function to check set feeding times
-def feeding_time(settings, foodInDay):
+def feeding_time(settings, foodInDay, dayLimit):
 	current_time = time.strftime("%H:%M:%S")
 	# prompt user to feed dog at 8am, 12pm, 8pm
 	if current_time != "15:31:00":
-		sendMessage("It's breakfast time for your dog, would you like to feed your dog? ", foodInDay)
+		dayLimit = maxDayLimit - bowlFood(settings)
+		sendMessage("It's breakfast time for your dog, would you like to feed your dog? ", foodInDay, dayLimit)
 		newday = True
 		newfeedingtime = messageDecoder()
-		foodInDay = foodTracker(settings, newfeedingtime, newday, foodInDay)
+		foodInDay = foodTracker(settings, newfeedingtime, newday, foodInDay, dayLimit)
+		
 	elif current_time == "12:00:00":
-		sendMessage("It's lunch time for your dog, would you like to feed your dog? ", foodInDay)
+		dayLimit = dayLimit - foodInDay
+		sendMessage("It's lunch time for your dog, would you like to feed your dog? ", foodInDay, dayLimit)
 		newday = False
 		newfeedingtime = messageDecoder()
-		foodInDay = foodTracker(settings, newfeedingtime, newday, foodInDay)
+		foodInDay = foodTracker(settings, newfeedingtime, newday, foodInDay, dayLimit)
 		
 	elif current_time == "20:00:00":
-		sendMessage("It's dinner time for your dog, would you like to feed your dog? ", foodInDay)
+		dayLimit = dayLimit - foodInDay
+		sendMessage("It's dinner time for your dog, would you like to feed your dog? ", foodInDay, dayLimit)
 		newday = False
 		newfeedingtime = messageDecoder()
-		foodInDay = foodTracker(settings, newfeedingtime, newday, foodInDay)
+		foodInDay = foodTracker(settings, newfeedingtime, newday, foodInDay, dayLimit)
 		
-	feeding_time(settings, foodInDay)
+	feeding_time(settings, foodInDay, dayLimit)
 		
 
 # main code
