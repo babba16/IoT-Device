931e10eab0d696b82a60755cf9046168e7384692
09tangriro <47002898+09tangriro@users.noreply.github.com>
2019-02-11 17:42:02 +0000

    lets test pls

--- src/main.py
+++ src/main.py
@@ -5,9 +5,10 @@ from messageDecoder import *
 
 def main():
 	foodInDay = 0
+	dayLimit = 3
 	settings = scalerSetting()
 	
-	feeding_time(settings, foodInDay)
+	feeding_time(settings, foodInDay, dayLimit)
 			
 if __name__ == "__main__":
     main()
