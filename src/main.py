601380d4ab4f5b847b2eac32f17eb82f60d50aa0
09tangriro <47002898+09tangriro@users.noreply.github.com>
2019-02-11 16:38:44 +0000

    needs testing

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 from feeding_time import *
 from dispensing import *
+from settings import *
 from messageDecoder import *
 
 def main():
-	newday = False
-	newfeedingtime = False
 	foodInDay = 0
+	settings = scalerSetting()
 	while 1:
-		feeding_time(newday,newfeedingtime,foodInDay)
+		feeding_time(settings, foodInDay)
 			
 if __name__ == "__main__":
     main()
