e404bacd548221f3c734738cba2fc4dcfb1e8282
09tangriro <47002898+09tangriro@users.noreply.github.com>
2019-02-13 14:45:00 +0000

    no daylimit

--- src/dispensing.py
+++ src/dispensing.py
@@ -6,7 +6,7 @@ mealLimit = 1
 
 bus.write_i2c_block_data(0x48,0x01,[0x88, 0x83]) # configure sensor
 
-def foodTracker(settings, newfeedingtime, newday, foodInDay, dayLimit):
+def foodTracker(settings, newfeedingtime, newday, foodInDay):
 	if newday == True:
 		print("It's a new day")
 		foodInDay = 0
@@ -17,7 +17,7 @@ def foodTracker(settings, newfeedingtime, newday, foodInDay, dayLimit):
 		#TODO: needs to be drive LED to represent motor
 		while  bowlFood(settings) < thisMeal:
 			print(bowlFood(settings)) #for testing, allows us to continuously see the value of the weight applied to sensor. 
-			time.sleep(5)
+			time.sleep(1)
 		foodInDay = foodInDay + thisMeal
 		
 	return foodInDay
