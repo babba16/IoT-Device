c0da64ac4dcc51f62bf84eab4ac66479f6945ca3
JT1316 <47001022+JT1316@users.noreply.github.com>
2019-02-13 16:36:46 +0000

    Update feeding_time.py

--- src/feeding_time.py
+++ src/feeding_time.py
@@ -3,38 +3,45 @@ from messageDecoder import *
 from dispensing import *
 import time
 
-maxDayLimit = 3
+#maxDayLimit = 3
 
 # function to check set feeding times
 def feeding_time(settings, foodInDay):
 	current_time = time.strftime("%H:%M:%S")
 	# prompt user to feed dog at 8am, 12pm, 8pm
 	if current_time != "15:31:00":
-		sendMessageStats("It's breakfast time for your dog, would you like to feed your dog? ", 0, maxDayLimit)
+		#send foodinday from previous day - food dispensed previous day was..
+		sendMessageStats(0, maxDayLimit) #"It's breakfast time for your dog, would you like to feed your dog? "
 		newday = True
 		newfeedingtime = messageDecoder()
 		foodInDay = foodTracker(settings, newfeedingtime, newday, foodInDay)
-		SendMessageMeal("Stats for today:", foodInDay)
+		SendMessageMeal(foodInDay) #food sidpensed breakfast
 		return foodInDay
 		
 	elif current_time == "12:00:00":
 		foodEaten = 1-bowlFood(settings)
 		foodLeft = maxDayLimit - foodEaten
-		sendMessageStats("It's lunch time for your dog, would you like to feed your dog? ", foodEaten, foodLeft)
+		sendMessageStats( foodEaten) #"It's lunch time for your dog, would you like to feed your dog? ", foofd eaten for breakfast
 		newday = False
 		newfeedingtime = messageDecoder()
 		foodInDay = foodTracker(settings, newfeedingtime, newday, foodInDay)
-		SendMessageMeal("Stats for this meal time, food dispensed and food left to be dispensed today:", foodInDay)
+		SendMessageMeal( foodInDay)#"Stats for this meal time, food dispensed and food left to be dispensed today:",
+		return foodInDay
+		
 		
 	elif current_time == "20:00:00":
-		foodEaten = 2-bowlFood(settings)
-		foodLeft = maxDayLimit - foodEaten
-		sendMessageStats("It's dinner time for your dog, would you like to feed your dog? ", foodEaten, foodLeft)
+		foodEaten = 1-bowlFood(settings)
+		#foodLeft = maxDayLimit - foodEaten
+		sendMessageStats( foodEaten) # food eaten is food eaten at lunch, "It's dinner time for your dog, would you like to feed your dog? ",
 		newday = False
 		newfeedingtime = messageDecoder()
 		foodInDay = foodTracker(settings, newfeedingtime, newday, foodInDay)
-		SendMessageMeal("Stats for this meal time, food dispensed and food left to be dispensed today:", foodInDay)
-		
+		SendMessageMeal( foodInDay)#"Stats for this meal time, food dispensed and food left to be dispensed today:"
+		return foodInDay
+	
+	
+	else:
+		return foodInDay
 	
 		
 
@@ -49,4 +56,3 @@ def feeding_time(settings, foodInDay):
 #	newfeedingtime = False
 	# poll function
 #	feeding_time(newday, newfeedingtime)
-
