878995a7d81ff9bb3567fd8a6a3af9627b5832f9
JT1316 <47001022+JT1316@users.noreply.github.com>
2019-02-13 16:36:06 +0000

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from messageDecoder import *
 
 def main():
 	foodInDay = 0
-	settings = scalerSetting()
+	settings = scalerSetting() #equal to one unit of food
 	
 	while 1:
 		foodInDay = feeding_time(settings, foodInDay)
